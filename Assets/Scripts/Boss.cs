6e1b5e5181d508c615637041f62692eed7d20b1a
Arumksh <clocknta@yahoo.co.jp>
2018-07-04 17:14:42 +0900

    ScriptTestプロジェクトの課題提出
    C#スクリプトはTestとBossの2つです。

---
+++ Assets/Scripts/Boss.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Boss : MonoBehaviour {
+
+    int hp;
+    int power;
+    int mp = 53;
+
+    private void Start()
+    {
+        var boss = new Boss();
+        for (int i = 0; i <= 10; i++)
+        {
+            boss.Magic(5);
+        }
+    }
+
+
+
+public void Attack()
+    {
+        Debug.Log(this.power + "のダメージを受けた");
+    }
+
+    public void Defend(int damage)
+    {
+        this.hp -= damage;
+        Debug.Log(damage + "のダメージを与えた");
+    }
+
+    public void Magic(int useMp)
+    {
+        if (mp >= useMp)
+        {
+            mp -= useMp;
+            Debug.Log("魔法攻撃をした。残りMPは" + mp);
+        }
+        else
+        {
+            Debug.Log("MPが足りないため魔法が使えない。");
+        }
+    }
+}
+
---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Test : MonoBehaviour {
+
+    void Start()
+    {
+        int[] array = {0, 1, 2, 3, 4};
+
+         for (int i = 0; i < array.Length; i++)
+         {
+             Debug.Log(array[i]);
+         }
+
+        for (int i = array.Length - 1; i >= 0; i--)
+        {
+            Debug.Log(array[i]);
+        }
+    }
+}
